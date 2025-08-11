7cbeebf0609c85f264e42c2907368b6fbc8d382e
Atharve03 <atharve.bute@gmail.com>
2025-08-11 18:28:00 +0530

    initial comit

---
+++ frontend/src/components/LatestCollection.jsx
@@ -0,0 +1,22 @@
+import React, { useContext, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from './Title';
+
+const LatestCollection = () => {
+    const {products} =useContext(ShopContext);
+    const [latestProducts,setLatestProducts] = useState([])
+ 
+  return (
+    <div className='my-10'>
+      <div className='text-center py-8 text-3xl'>
+        <Title text1={'LATEST'} text2={"COLLECTIONS"}/>
+        <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'> 
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam omnis quidem rem impedit laboriosam. Ipsum eaque, rem nam voluptatum similique illum necessitatibus corporis pariatur quaerat? Illum, facere. Nisi, totam recusandae!
+        </p>
+      </div>
+
+    </div>
+  )
+}
+
+export default LatestCollection
