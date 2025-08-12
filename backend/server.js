c1a7ab57cf45e1461db15135c87c21ccbc91add5
thunderavi <hittuprasad0909@gmail.com>
2025-08-12 21:39:44 +0530

    added server local port

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 8000;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/',(req,res)=>{
+    res.send(`Server is running on port 8000`);
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
